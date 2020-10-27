5edfb2ed843750e8d13e15c1c04ba5326e689ebf
MEianelli <filipelouzano@gmail.com>
2020-10-27 18:24:23 -0300

    Updates

---
+++ precache-manifest.e9acd0d10154b5a361c013243577d27e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7109a10cdf14ea58d25aa99a63714038",
+    "url": "/projeto-avoa/index.html"
+  },
+  {
+    "revision": "997351b98cd83127df89",
+    "url": "/projeto-avoa/static/css/main.72341a11.chunk.css"
+  },
+  {
+    "revision": "62f38a8199ea094b9513",
+    "url": "/projeto-avoa/static/js/2.e7e46dfa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/projeto-avoa/static/js/2.e7e46dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997351b98cd83127df89",
+    "url": "/projeto-avoa/static/js/main.c06eac95.chunk.js"
+  },
+  {
+    "revision": "ea0cf21754daad1a8d73",
+    "url": "/projeto-avoa/static/js/runtime-main.0cfcb33e.js"
+  },
+  {
+    "revision": "2acde4df38a96fb1b3a838325b627f87",
+    "url": "/projeto-avoa/static/media/foto1.2acde4df.jpg"
+  },
+  {
+    "revision": "fcd0aaf58fe56a8c8beaf68c098e6136",
+    "url": "/projeto-avoa/static/media/foto10.fcd0aaf5.jpg"
+  },
+  {
+    "revision": "349ca3fb02fe028ce672ddeb0cb3c5b1",
+    "url": "/projeto-avoa/static/media/foto11.349ca3fb.jpg"
+  },
+  {
+    "revision": "87f096c4b9d9062222eda3d308c78fa3",
+    "url": "/projeto-avoa/static/media/foto12.87f096c4.jpg"
+  },
+  {
+    "revision": "85cd7f30ae2a9d0b856275af83596ef1",
+    "url": "/projeto-avoa/static/media/foto13.85cd7f30.jpg"
+  },
+  {
+    "revision": "bfb470c2b4edbadb7d82803f4c9e54b2",
+    "url": "/projeto-avoa/static/media/foto14.bfb470c2.jpg"
+  },
+  {
+    "revision": "762babe115673e4cce6e4389df14bf43",
+    "url": "/projeto-avoa/static/media/foto15.762babe1.jpg"
+  },
+  {
+    "revision": "bd0c39ab5b38aa2e43a153ca956b826e",
+    "url": "/projeto-avoa/static/media/foto16.bd0c39ab.jpg"
+  },
+  {
+    "revision": "06a14e3f6cd42da6d6eeb0e541b384aa",
+    "url": "/projeto-avoa/static/media/foto17.06a14e3f.jpg"
+  },
+  {
+    "revision": "3e9934a9c1909747e47e40474812ab24",
+    "url": "/projeto-avoa/static/media/foto18.3e9934a9.jpg"
+  },
+  {
+    "revision": "ab208622c26382dbfcc15380ecbdf8f2",
+    "url": "/projeto-avoa/static/media/foto2.ab208622.jpg"
+  },
+  {
+    "revision": "66d7813fe3fd631d35fee245578dedc3",
+    "url": "/projeto-avoa/static/media/foto3.66d7813f.jpg"
+  },
+  {
+    "revision": "42d4cb0106ed5e003098a9b9cc9acc9c",
+    "url": "/projeto-avoa/static/media/foto4.42d4cb01.jpg"
+  },
+  {
+    "revision": "8e6f6d5db96e1a9544627288d6c2ad0a",
+    "url": "/projeto-avoa/static/media/foto5.8e6f6d5d.jpg"
+  },
+  {
+    "revision": "fdc453117746a0e2442c76ebbf8a0e08",
+    "url": "/projeto-avoa/static/media/foto6.fdc45311.jpg"
+  },
+  {
+    "revision": "b7a9a3b2f5b04e42c1ae26fb75cec9ba",
+    "url": "/projeto-avoa/static/media/foto7.b7a9a3b2.jpg"
+  },
+  {
+    "revision": "d8688197ce30fb2a8fcf2eac0066aee7",
+    "url": "/projeto-avoa/static/media/foto8.d8688197.jpg"
+  },
+  {
+    "revision": "b9df9b29615ab30628c40fa186190ea9",
+    "url": "/projeto-avoa/static/media/foto9.b9df9b29.jpg"
+  }
+]);
